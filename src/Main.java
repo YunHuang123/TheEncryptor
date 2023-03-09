258f4ce4c0e04fe74325d7fbe30fb16636bc24d4
YunHuang123 <114007330+YunHuang123@users.noreply.github.com>
2023-03-09 17:42:22 -0500

    Finished

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Encryptor encryptor1 = new Encryptor(2, 2);
+        System.out.println(encryptor1.decryptMessage("Il apy dveio mgeas.AAAAA"));
+    }
+}
